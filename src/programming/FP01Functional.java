d12bec971b78b705d98907ae15248af1990d953d
Ravi Ranjan <raviraz73@gmail.com>
2024-02-09 08:25:49 +0530

    Improving Java Functional Program with filter

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -6,16 +6,31 @@ public class FP01Functional {
 
 	public static void main(String[] args) {
 		
-		printAllNumbersInListFunctional(List.of(1, 2, 3, 4, 5));
+		List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+		printAllNumbersInListFunctional(numbers);
+		printEvenNumbersInListFunctional(numbers);
 
 	}
 	
-	public static void print(int number) {
-		System.out.println(number);
-	}
+	/*
+	 * public static void print(int number) { System.out.println(number); }
+	 */
 
+	private static boolean isEven(int number) {
+		return number%2==0;
+	}
+	
 	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
-		numbers.stream().forEach(FP01Functional::print);//Reference method
+		//numbers.stream().forEach(FP01Functional::print);//Reference method
+		numbers.stream().forEach(System.out::println);
+		
+	}
+	
+	private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+		//numbers.stream().forEach(FP01Functional::print);//Reference method
+		numbers.stream()
+		.filter(FP01Functional::isEven)
+		.forEach(System.out::println);
 		
 	}
 
--- src/programming/FP01Structured.java
+++ src/programming/FP01Structured.java
@@ -6,7 +6,10 @@ public class FP01Structured {
 
 	public static void main(String[] args) {
 		
-		printAllNumbersInListStructured(List.of(1, 2, 3, 4, 5));
+		//printAllNumbersInListStructured(List.of(1, 2, 3, 4, 5));//Extract value to local value:Alt+Shift+L
+		List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+		printAllNumbersInListStructured(numbers);
+		printEvenNumbersInListStructured(numbers);
 
 	}
 
@@ -15,6 +18,13 @@ public class FP01Structured {
 			System.out.println(number);
 		}
 		
+	}
+	
+	private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+		for(Integer number:numbers) {
+			if(number%2==0)
+				System.out.println(number);
+		}
 	}
 
 }
